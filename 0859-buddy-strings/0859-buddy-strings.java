5885c3fb8e8d8243643c0bf2e41b9fc5e643e2d0
Shreyash2910 <101186513+Shreyash2910@users.noreply.github.com>
2026-08-06 00:38:14 +0530

    Time: 1 ms (99.55%), Space: 44 MB (29.23%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,27 +1,41 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
-        HashSet<Character> set= new HashSet<>();
-        if(s.length() != goal.length())
+        if(s.length()!=goal.length()){
             return false;
+        }
+        if (s.equals(goal)) {
+            HashSet<Character> set = new HashSet<>();
+
+            for (char ch : s.toCharArray()) {
+                if (set.contains(ch)) {
+                    return true;   
+                }
+                set.add(ch);
+            }
+
+            return false;
+        }
+        int first=-1;
+        int second =-1;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!=goal.charAt(i)){
+                if(first==-1){
+                    first=i;
+                }
+                else if(second==-1){
+                    second=i;
+                }
+                else{
+                    return false;
+                }
+            }
+            
+        }
+        return (second != -1 &&
+               s.charAt(first) == goal.charAt(second) &&
+               s.charAt(second) == goal.charAt(first));
         
-        if(s.equals(goal)){
-            for(char ch: s.toCharArray()){
-            set.add(ch);
-            }
-            return s.length()>set.size();
-        }
-        List<Integer> notequal = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != goal.charAt(i)) {
-                notequal.add(i);
-            }
-        }
-        if(notequal.size()!=2)
-            return false;
-        int i= notequal.get(0);
-        int j= notequal.get(1);
-        if(s.charAt(i) == goal.charAt(j) && s.charAt(j) == goal.charAt(i))
-            return true;
-        return false;
+
     }
 }
