5b24d6861af07a815772d0aa8d224b9808494b9f
Shreyash2910 <101186513+Shreyash2910@users.noreply.github.com>
2026-08-11 13:48:12 +0530

    Time: 4 ms (71.36%), Space: 54.9 MB (86.62%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result= new StringBuilder();
+        for(int i =0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(ch>96 && ch<123 ){
+                result.append(ch);
+            }
+            else if(ch=='#'){
+                result.append(result);
+            }
+            else if(ch=='%'){
+                result.reverse();
+            }
+            else if(ch=='*'&& result.length()>0){
+                result.deleteCharAt(result.length()-1);
+            }
+        }
+        return result.toString();
+    }
+}
