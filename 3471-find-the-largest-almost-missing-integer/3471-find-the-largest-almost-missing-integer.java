5c698d49196160e45990376ad8e7a472c01c82dd
Shreyash2910 <101186513+Shreyash2910@users.noreply.github.com>
2026-08-19 01:57:06 +0530

    Time: 1 ms (99.3%), Space: 45.2 MB (47.72%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        int[] count = new int[51];
+        for (int i = 0; i <= n - k; i++) {
+            boolean[] seen = new boolean[51];
+
+            for (int j = i; j < i + k; j++) {
+
+                int x = nums[j];
+
+                if (!seen[x]) {
+                    count[x]++;
+                    seen[x] = true;
+                }
+            }
+        }
+        int answer = -1;
+        for (int x = 0; x <= 50; x++) {
+            if (count[x] == 1) {
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+}
