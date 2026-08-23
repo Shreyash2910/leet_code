bc229c8caa4f64490039f8b7785f99116b9ebbdd
Shreyash2910 <101186513+Shreyash2910@users.noreply.github.com>
2026-08-23 14:52:25 +0530

    Time: 35 ms (43.02%), Space: 58.7 MB (93.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,7 +8,6 @@ class Solution {
         Arrays.sort(nums); 
         
         for (int i = 0; i < nums.length - 2; i++) { 
-            // 1. Fixed typo: changed 'num' to 'nums'
             if (i > 0 && nums[i] == nums[i - 1]) 
                 continue; 
             int left = i + 1; 
